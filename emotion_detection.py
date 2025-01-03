43774030a1cd86f598a98f4088765d56b3d99e5d
Sanders-NLPAI <sanna.sanders.de@gmail.com>
2025-01-03 14:11:14 -0500

    task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,6 @@
 """API module for Watson NLP Libraries
 """
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -9,4 +10,22 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url,json=input_json,headers=headers, timeout=3)
-    return response.text
+    # convers response to json format
+    res_json = json.loads(response.text)
+
+    # extract the set of emotions with corresponding scores
+    emotion_scores = res_json["emotionPredictions"][0]["emotion"] #NOTE: unnecessary nested json structure: list with one dictionary
+
+    #find dominant emotion
+    scores_sorted = sorted(emotion_scores.items(), key=lambda x:x[1], reverse=True)
+    dominant_emotion = scores_sorted[0][0]
+    
+    formatted_output ={
+        'anger': emotion_scores["anger"],
+        'disgust': emotion_scores["disgust"],
+        'fear': emotion_scores["fear"],
+        'joy': emotion_scores["joy"],
+        'sadness': emotion_scores["sadness"],
+        "dominant_emotion" : dominant_emotion
+    }
+    return formatted_output
